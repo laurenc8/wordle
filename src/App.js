185a0cfcc20e7d19281e11a94c46c816744ca020
Ben Kang <benkang@MacBook-Pro-5.local>
2022-01-26 20:13:50 -0500

    python can play now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import GridLayout from './components/GridLayout';
 import { useState } from 'react';
+import $ from 'jquery'
 
 function App() {
   const [guessNum, setGuessNum] = useState(0)
@@ -22,26 +23,92 @@ function App() {
   function handleKeyDown(event) {
     const currentGuess = (guessNum === 0 ? guess0 : (guessNum === 1 ? guess1 : (guessNum === 2 ? guess2 : (guessNum === 3 ? guess3 : (guessNum === 4 ? guess4 : guess5)))))
     if (event.key === 'Enter' && currentGuess.length === 6) {
-      setGuessNum(guessNum + 1)
-      $.post('http://127.0.0.1:5000/test', {
-        result: "rgygrg"
-      }).then(function (response) {
-        console.log(response)
-      });
+      // setGuessNum(guessNum + 1)
+      let goalWord = "asdfgh"
+      let result = getResult(goalWord, currentGuess)
+    }
+  }
+  
+  function autoPlay() {
+    setGuess0("")
+    setGuess1("")
+    setGuess2("")
+    setGuess3("")
+    setGuess4("")
+    setGuess5("")
+    fetch('http://127.0.0.1:5000/random',
+      {
+        method: 'GET',
+        mode: 'cors'
+      }
+    ).then(function (response)  {
+      return response.json();
+    }).then(function (text) {
+      console.log(text.word)
+      return text.word
+    }).then(function (goalWord) {
       fetch('http://127.0.0.1:5000/test',
         {
           method: 'GET',
           mode: 'cors'
         }
-      )
-        .then(function (response)  {
-          return response.json();
-        })
-        .then(function (text) {
-          console.log(text.greeting)
-          setResp(text.greeting)
-        });
+      ).then(function (response)  {
+        return response.json();
+      }).then(function (text) {
+        console.log(text.guess)
+        setGuess0(text.guess.toUpperCase())
+        return text.guess
+      }).then(function (guess) {
+        getWordFromPython(goalWord, guess, 1)
+      });
+    })
+  }
+
+  function getWordFromPython(goalWord, guess, depth) {
+    if(depth < 6 && guess !== goalWord) {
+      let result = getResult(goalWord, guess)
+      console.log(result)
+      $.post('http://127.0.0.1:5000/test', {
+        result: result
+      }).then(function (nextGuess) {
+        console.log(depth)
+        console.log(nextGuess)
+        const currentSetter = depth === 0 ? setGuess0 : (depth === 1 ? setGuess1 : (depth === 2 ? setGuess2 : (depth === 3 ? setGuess3 : (depth === 4 ? setGuess4 : setGuess5))))
+        currentSetter(nextGuess.toUpperCase())
+        return nextGuess
+      }).then(function (nextGuess) {
+        getWordFromPython(goalWord, nextGuess, depth + 1)
+      });
     }
+    else if(guess === goalWord) {
+      console.log("yay")
+    }
+    else {
+      console.log("trash")
+    }
+  }
+
+  function getResult(goalWord, guess) {
+    let matched = new Set()
+    let result = ""
+    for(let j = 0; j < 6; j++){
+      if(guess.charAt(j) === goalWord.charAt(j)){
+        result += "g"
+        matched.add(j)
+      }
+      else {
+        let yellow = false
+        for(let l = 0; l < 6; l++){
+          if(goalWord.charAt(l) === guess.charAt(j) && !matched.has(l) && goalWord.charAt(l) !== guess.charAt(l)){
+            yellow = true
+            matched.add(l)
+            break
+          }
+        }
+        result += yellow ? "y" : "r"
+      }
+    }
+    return result
   }
 
   return (
@@ -57,6 +124,11 @@ function App() {
         onKeyDown={handleKeyDown}
         style={{margin: '20px'}}
       />
+      <button
+        type="button"
+        onClick={autoPlay}
+      > Let Python Play
+      </button>
       <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <GridLayout guesses = {[guess0, guess1, guess2, guess3, guess4, guess5]}/>
       </div>
--- src/bot.py
+++ src/bot.py
@@ -9,20 +9,26 @@ app = Flask(__name__)
 #     return jsdata + " hi"
 
 @app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    # GET request
+def send_next_word():
     if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers
-    # POST request
+        message = {'guess': prev_words[-1]}
+        return jsonify(message)
     if request.method == 'POST':
-        print(request.form['result'])  # parse as JSON
-        filter_words(words, word, request.form['result'])
-        return next_word(words), 200
-        #return "hello", 200
+        filter_words(words, prev_words[-1], request.form['result'])
+        print(words)
+        print(request.form['result'])
+        print(len(words))
+        prev_words.append(next_word())
+        return prev_words[-1], 200
+
+@app.route('/random', methods=['GET'])
+def generate_random_word():
+    setup(6)
+    prev_words.append('praise')
+    message = {'word': next_word()}
+    return jsonify(message)  # serialize and use JSON headers
 
 def setup(word_len):
-    words = set()
     with open("words_all.txt") as file:
         while True:
             line = file.readline().strip()
@@ -30,7 +36,6 @@ def setup(word_len):
                 break
             if len(line) == word_len:
                 words.add(line)
-    return words
 
 
 def filter_words(words, prev_word, result):
@@ -40,12 +45,10 @@ def filter_words(words, prev_word, result):
         letter = prev_word[i]
         if letter not in restrictions:
             restrictions[letter] = {}
-            restrictions[letter]['good'] = set()
             restrictions[letter]['bad'] = set()
             restrictions[letter]['freq'] = 0
             restrictions[letter]['freqKnown'] = False
         if result[i] == 'g':
-            restrictions[letter]['good'].add(i)
             restrictions[letter]['freq'] += 1
         elif result[i] == 'y':
             restrictions[letter]['bad'].add(i)
@@ -64,35 +67,28 @@ def fail(word, prev_word, result, restrictions):
         letter = word[i]
         if result[i] == 'g' and letter != prev_word[i]:
             return True
-        if i in restrictions:
+        if letter in restrictions:
             if i in restrictions[letter]['bad']:
                 return True
             if letter not in frequencies:
                 frequencies[letter] = 1
             else:
                 frequencies[letter] += 1
-            if frequencies[letter] > restrictions[letter]['freq'] and restrictions['freqKnown']:
+            if frequencies[letter] > restrictions[letter]['freq'] and restrictions[letter]['freqKnown']:
                 return True
-        for letter in frequencies:
-            if frequencies[letter] < restrictions[letter]['freq']:
-                return True
+    for letter in restrictions:
+        if letter not in frequencies:
+            frequencies[letter] = 0
+        if frequencies[letter] < restrictions[letter]['freq']:
+            return True
     return False
 
 
-def next_word(words):
+def next_word():
     return random.sample(words,1)[0]
 
 
 if __name__ == "__main__":
-    words = setup(6)
-    word = "praise"
+    words = set()
+    prev_words = []
     app.run()
-    # word_len = int(input("Length of word: "))
-    # words = setup(word_len)
-    # for guess in range(6):
-    #     guess = next_word(words)
-    #     print(guess)
-    #     result = input("Result? Green = G, Yellow = Y, Gray = R")
-    #     filter_words(words, guess, result)
-
-    # print(filter_words(setup(5), "raise", "rgryg"))
